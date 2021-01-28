e6ffe2271be762c520abba027ce738c3784e2de9
aalassi <aalassi@yahoo.com>
2021-01-28 23:46:37 +0100

    update form till here

---
+++ restaurantform.js
@@ -0,0 +1,66 @@
+
+
+const reservationForm = document.getElementById("my-reservation-form")
+
+reservationForm.addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const url = form.action;
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
+        console.log({ responseData });
+    } catch (error) {
+		console.error(error);
+	}
+}
+
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+
+	const fetchOptions = {
+        method: "POST",
+        headers: {
+			"Content-Type": "application/json",
+            "Accept": "application/json"},
+            body: formDataJsonString,
+        };
+
+        const response = await fetch(url, fetchOptions);
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+        }
+    
+        return response.json();
+    }
+
+
+
+/*
+[
+     
+   let reservation = [{
+       id: 1,
+       name: document.getElementById("name").value,
+       phone: document.getElementById("phone").value,
+       nnumberOfPersons: document.getElementById("numberOfPersons").value,
+       smoking:document.getElementById("smoking").value,
+       datetime: document.getElementById("dateTime").value,
+       specialReq : document.getElementById("specialRequests").value
+   }
+
+
+
+
+
+
+
+
+
+
+
+] */
