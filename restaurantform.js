e2c04ee9c6ca20ac7a7f0d394d0a00d67680a90f
aalassi <aalassi@yahoo.com>
2021-02-02 16:44:20 +0100

    added highlight scroll code

--- restaurantform.js
+++ restaurantform.js
@@ -232,6 +232,63 @@ console.log(search)
 
 
 
+ // highlight menu items when scroll starts 
+
+ var sections = document.querySelectorAll("section");
+
+ onscroll = function () {
+   var scrollPosition = document.documentElement.scrollTop;
+ 
+   sections.forEach((section) => {
+     if (
+       scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+       scrollPosition <
+         section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+     ) {
+       var currentId = section.attributes.id.value;
+       removeAllActiveClasses();
+       addActiveClass(currentId);
+     }
+   });
+ };
+ 
+ var removeAllActiveClasses = function () {
+   document.querySelectorAll(".w3-top a").forEach((el) => {
+     el.classList.remove("active2");
+   });
+ };
+ 
+ var addActiveClass = function (id) {
+   // console.log(id);
+   var selector = `.w3-top a[href="#${id}"]`;
+   document.querySelector(selector).classList.add("active2");
+ };
+ 
+ var navLinks = document.querySelectorAll(".w3-top a");
+ 
+ navLinks.forEach((link) => {
+   link.addEventListener("click", (e) => {
+     e.preventDefault();
+     var currentId = e.target.attributes.href.value;
+     var section = document.querySelector(currentId);
+     var sectionPos = section.offsetTop;
+     // section.scrollIntoView({
+     //   behavior: "smooth",
+     // });
+ 
+     window.scroll({
+       top: sectionPos,
+       behavior: "smooth",
+     });
+   });
+ });
+ 
+
+//highlight ends 
+
+
+
+
 
 
 
