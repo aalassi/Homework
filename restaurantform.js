b64c79550f2ce91d03b7a55fa61a4a58c948e184
aalassi <aalassi@yahoo.com>
2021-02-03 22:10:16 +0100

    removing button up

--- restaurantform.js
+++ restaurantform.js
@@ -230,6 +230,36 @@ console.log(search)
 }
  )
 
+//scroll up button function starts 
+
+
+//Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  }
+  else if ($(window).width() <= 767)  {    mybutton.style.display = "none";
+} 
+  
+  else {
+    mybutton.style.display = "none";
+  }
+ 
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// scroll up button function ends 
+
 
 
  // highlight menu items when scroll starts 
@@ -287,36 +317,6 @@ console.log(search)
 //highlight ends 
 
 
-
-//scroll up button function starts 
-
-
-//Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  }
-  else if ($(window).width() <= 767)  {    mybutton.style.display = "none";
-} 
-  
-  else {
-    mybutton.style.display = "none";
-  }
- 
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-// scroll up button function ends 
 
 
 
