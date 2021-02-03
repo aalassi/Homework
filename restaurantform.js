dbf9696e6e6cc1740e572bf7761171e64bec9c2f
aalassi <aalassi@yahoo.com>
2021-02-03 19:02:42 +0100

    edited code up button

--- restaurantform.js
+++ restaurantform.js
@@ -292,7 +292,7 @@ console.log(search)
 
 
 //Get the button:
-mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
